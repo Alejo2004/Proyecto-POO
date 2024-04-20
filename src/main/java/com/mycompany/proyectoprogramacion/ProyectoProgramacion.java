bf78935596e02529d7c89388b79f0db14cff25e6
Alejandro Largo <alejandrolargo220@gmail.com>
2024-04-20 15:45:07 -0500

    Proyecto con Interfaz

---
+++ src/main/java/com/mycompany/proyectoprogramacion/ProyectoProgramacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoprogramacion;
+
+/**
+ *
+ * @author Sidonia Morales
+ */
+public class ProyectoProgramacion {
+
+    public static void main(String[] args) {
+        
+    }
+}
